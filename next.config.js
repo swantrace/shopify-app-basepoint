ee2ee02591fafc8b1166bb86194a9c265076fe99
Qixuan Hong <frederickhong.5@gmail.com>
2021-06-28 18:50:21 -0700

    ignore eslint

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ module.exports = {
   env: {
     SHOPIFY_API_KEY: process.env.SHOPIFY_API_KEY,
   },
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
